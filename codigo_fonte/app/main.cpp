7e0e9073fc57dda91ec0f6ec7dc0eb305247b052
Luiz <luizcn123@hotmail.com>
2023-01-12 22:40:46 -0300

    adiciona files :add:

--- codigo_fonte/app/main.cpp
+++ codigo_fonte/app/main.cpp
@@ -1,20 +1,46 @@
-#include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
-int main(int argc, char **argv)
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    if(argc < 2){
-        printf("Imagem nao encontrada\n");
+    // Carrega o arquivo de imagem
+    Mat frame = imread(argv[1]);
+    if (frame.empty())
+    {
+        cout << "Não foi possível carregar a imagem" << endl;
         return -1;
     }
-    Mat image;
-    image = imread(argv[1]);
-    if(!image.data){
-        printf("Imagem nao encontrada\n");
+
+    // Carrega o classificador de cascata para detectar placas de carro
+    CascadeClassifier car_cascade("../cascade_files/car_cascade.xml");
+    if (car_cascade.empty())
+    {
+        cout << "Não foi possível carregar o classificador de cascata" << endl;
+        return -1;
     }
-    namedWindow(argv[1], WINDOW_AUTOSIZE);
-    imshow(argv[1], image);
+
+    // Convert imagem para escala de cinza
+    Mat gray;
+    cvtColor(frame, gray, COLOR_BGR2GRAY);
+    equalizeHist(gray, gray);
+
+    // Detecta as placas de carro na imagem
+    vector<Rect> cars;
+    car_cascade.detectMultiScale(gray, cars);
+
+    // Desenha retângulos em torno das placas detectadas
+    for (size_t i = 0; i < cars.size(); i++)
+    {
+        rectangle(frame, cars[i], Scalar(0, 0, 255), 2);
+    }
+
+    // Exibe a imagem com as placas detectadas
+    imshow("Placas de carro detectadas", frame);
     waitKey(0);
+
     return 0;
 }
