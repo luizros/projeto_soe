0aeabc475c7fedce7520d34ac1e5f3b10ec66583
Luiz <luizcn123@hotmail.com>
2023-02-03 14:31:57 -0300

    altera projeto

--- codigo_fonte/app/main.cpp
+++ codigo_fonte/app/main.cpp
@@ -5,60 +5,74 @@
 #include "ler_o_arquivo.h"
 #include <string>
 #include <stdio.h>
+#include <iostream>
 
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    // Carrega o arquivo de imagem
-    Mat frame = imread(argv[1]);
-    Mat save_image;
-    char placa[10];
-    if (frame.empty())
+    // Abra a webcam
+    VideoCapture cap(-1);
+
+    // Verifique se a webcam foi aberta com sucesso
+    if (!cap.isOpened())
     {
-        cout << "Não foi possível carregar a imagem" << endl;
+        cout << "Erro ao abrir a webcam" << endl;
         return -1;
     }
 
-    // Carrega o classificador de cascata para detectar placas de carro
-    CascadeClassifier car_cascade("../cascade_files/car_cascade.xml");
-    if (car_cascade.empty())
+    // Crie uma janela para exibir o vídeo
+    namedWindow("Placa de Veiculo", WINDOW_NORMAL);
+
+    // Crie um objeto de classificador de placa de veículo
+    CascadeClassifier placa_cascade;
+    string caminho_classificador = "../cascade_files/car_cascade.xml";
+
+    // Carregue o classificador de placa
+    if (!placa_cascade.load(caminho_classificador))
     {
-        cout << "Não foi possível carregar o classificador de cascata" << endl;
+        cout << "Erro ao carregar classificador de placa" << endl;
         return -1;
     }
 
-    // Convert imagem para escala de cinza
-    Mat gray;
-    cvtColor(frame, gray, COLOR_BGR2GRAY);
-    equalizeHist(gray, gray);
-
-    // Detecta as placas de carro na imagem
-    vector<Rect> cars;
-    car_cascade.detectMultiScale(gray, cars);
-
-    // Desenha retângulos em torno das placas detectadas
-    for (size_t i = 0; i < cars.size(); i++)
+    while (true)
     {
-        cout << cars[i] << endl;
-        rectangle(frame, cars[i], Scalar(0, 255, 255), 2);
-        imwrite("../images_and_videos/images/test.jpg", frame); 
-        // Corte o retângulo da imagem original
-        Mat objects = frame(cars[i]);
+        // Capture o quadro atual da webcam
+        Mat frame;
+        cap >> frame;
 
-        stringstream ss;
-        ss << "../images_and_videos/images/placa_" << i << ".jpg";
-        
-        escrever("jhy4525");
-        escrever("asf2563");
-        imwrite(ss.str(), objects);
-        ler_placas();
+        // Converte o quadro para escala de cinza
+        Mat frame_gray;
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+
+        // Equalize o histograma do quadro em escala de cinza
+        equalizeHist(frame_gray, frame_gray);
+
+        // Detecte as placas de veículo no quadro
+        vector<Rect> placas;
+        placa_cascade.detectMultiScale(frame_gray, placas, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+        // Desenhe retângulos ao redor das placas detectadas
+        for (size_t i = 0; i < placas.size(); i++)
+        {
+            rectangle(frame, placas[i], Scalar(0, 0, 255), 2, 8, 0);
+        }
+
+        // Exiba o quadro com as placas detectadas
+        imshow("Placa de Veiculo", frame);
+
+        // Interrompa o loop se a tecla 'q' for pressionada
+        if (waitKey(10) == 'q')
+        {
+            break;
+        }
     }
-    
-    // Exibe a imagem com as placas detectadas
-    imshow("JHY7532", frame);
-    waitKey(0);
+    escrever("JHY7525");
+    // Feche a webcam e a janela
+    cap.release();
+    destroyAllWindows();
+
     return 0;
 }
